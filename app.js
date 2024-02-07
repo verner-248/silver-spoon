ab3486d3b286de4e568fa19cb930e20d4f9cb997
verner-248 <78090114+verner-248@users.noreply.github.com>
2024-02-08 01:08:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const container=document.querySelector(".container");
+const prop=document.querySelector("#proposal");
+const gif=document.querySelector("#gif");
+const yes_btn=document.querySelector("#yes");
+const no_btn=document.querySelector("#no");
+
+yes_btn.addEventListener("click", () =>{
+    prop.innerHTML="Yayyeeeeee😍😍, Now get ready for my love beam";
+     gif.src = "assets/kiss.gif";
+});
+
+no_btn.addEventListener("mouseover", () =>{
+    const nobtnrect=no_btn.getBoundingClientRect();
+    const maxX=window.innerWidth-nobtnrect.width;
+    const maxY=window.innerHeight-nobtnrect.height;
+
+    const randomX=Math.floor(Math.random()*maxX);
+    const randomY=Math.floor(Math.random()*maxY);
+
+    no_btn.style.left=randomX + "px";
+    no_btn.style.top=randomY + "px";
+})
